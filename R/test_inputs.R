f18cb16d09522d2c08f4848a859d69fc4f8a17b3
VEZY Rémi <vezy.remi@gmail.com>
2018-11-08 18:42:41 +0100

    Add tests for Area

---
+++ R/test_inputs.R
@@ -0,0 +1,39 @@
+#' Test the Area data.frame
+#'
+#' @description Performs several tests on the Area file to check for potential errors or typos
+#'
+#' @param Area      The Area data.frame
+#'
+#' @return `TRUE` if the file had no major problem, or a special output that points to the potential
+#' error
+#' @importFrom dplyr group_by "%>%" arrange summarise filter mutate
+#'
+#' @examples
+#'\dontrun{
+#' library(Vpalmr)
+#' Area= data.table::fread('LeafArea_monitoring_SMSE.csv',data.table = F,dec=',')
+#' test_Area(Area= Area)
+#'}
+#' @export
+test_Area= function(Area){
+  # Testing that the number of leaflets and their rank are the same in each
+  # section of the leaf:
+  Test=
+    Area%>%
+    group_by(TreeNumber,LeafIndex,Section)%>%
+    arrange(.by_group = T)%>%
+    summarise(LeafletRankOnSection= length(unique(LeafletRankOnSection)),
+              NbLeaflets= length(unique(NbLeaflets)))%>%
+    filter(NbLeaflets>1|LeafletRankOnSection>1)%>%
+    mutate(LeafletRankOnSection= ifelse(LeafletRankOnSection>1,"Problem","OK"),
+           NbLeaflets= ifelse(NbLeaflets>1,"Problem","OK"))
+
+  if(nrow(Test)>0){
+    message("Problem on input Area data: wrong value")
+    message(paste("Please check that the number of leaflets and their rank",
+                  "are the same in each section of the leaves mentionned"))
+    return(Test)
+  }else{
+    return(TRUE)
+  }
+}
